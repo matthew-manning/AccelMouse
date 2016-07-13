8181fc259485488bb56d7463b6f9ab2b260cc52e
matthew manning <mbhmanning@gmail.com>
2016-07-13 20:15:41 +1200

    regular commit

--- i2cRead.c
+++ i2cRead.c
@@ -3,6 +3,9 @@
 */
 
 #include <stdio.h>
+
+#define ACCEL_ADDR 0x68 //i2c addr for accelrometer 
+
 #define ACCEL_DIVSOR 8192 //for +- 4g
 #define ACCEL_CONFIG 0x08
 #define ACCEL_CON_REG 0x1c
@@ -10,10 +13,19 @@
 #define X_ACCEL_REG1 0x3b//reg for bits 15-8 of x acceleration
 #define X_ACCEL_REG2 0x3c//^ bits 7-0
 
+int DevHandle; 
+
+
 int main(void)
 {
 	//open i2c 
-
+	DevHandle = open("/dev/i2c-1", 0_RDWR);
+	
+	if(DevHandle < 0)//check handle is okay
+	{
+		printf("failed to open i2c handle\n");
+		return -1;
+	}
 
 	//write config registers
 	
