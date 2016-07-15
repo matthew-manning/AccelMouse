1ec2e0283057d880732e3ec84b799479a0c5e103
matthew manning <mbhmanning@gmail.com>
2016-07-16 10:26:52 +1200

    regular commit

--- i2cRead.c
+++ i2cRead.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <linux/i2c-dev.h>
 #include <fcntl.h>//open function
+#include <sys/ioctl.h>//ioctl function 
+#include <unistd.h>//read function
 #include <stdint.h>
 
 #define ACCEL_ADDR 0x68 //i2c addr for accelrometer 
@@ -17,6 +19,7 @@
 #define X_ACCEL_REG2 0x3c//^ bits 7-0, think not needed for this
 
 int DevHandle; 
+int NumRead;//debug value	
 uint8_t Reg16b[3];//array for reading/writing 16 bits (2 registers)
 float XAccel; // m/s/s value stored
 float AccScale = ACCEL_DIVSOR/9.80665; 
@@ -46,22 +49,26 @@ int main(void)
 		//read registers
 		Reg16b[0] = X_ACCEL_REG1; //setting address
 		
-		if( read(Reg16b, 2) != 2)//return Num reg read should be same as requsted Num
+		NumRead = read(DevHandle, Reg16b, 2);//debug line
+		
+		if( NumRead !=2)//read(Reg16b, 2) != 2)//return Num reg read should be same as requsted Num
 		{
-			printf("could not read reg %d\n", X_ACCEL_REG1);
+			printf("number of regs read is: %d; Reg16b[1] is: %x, REg16b[2] is: %x\n", NumRead, Reg16b[1], Reg16b[2]);
+			printf("could not read reg %x\n", X_ACCEL_REG1);
 			return -1;
 		}
 		
 		
 		
 		//get sensible values and print
-		XAccel = (( (Reg16b[2] << 8) | Reg16b[3]) / AccScale);/*Unkowns
+		XAccel = (( (Reg16b[1] << 8) | Reg16b[0]) / AccScale);/*Unkowns
 															  **negitive values' minus sign properly interpreted?
 															  *
-															  **is the 15-8 bits stored in Reg16b[2] and not Reg16b[3]?
+															  **is the 15-8 bits stored in Reg16b[1] and not Reg16b[2]?
 															  */
 		printf("acceleration measured is %0.2f ms^-2\n", XAccel);
 		
+		usleep(250000);//250 ms
 	}
 	
 	return 0;
