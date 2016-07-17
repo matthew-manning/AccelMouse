887246bf9cfcffaa0b744c798e8e020f60997cd7
matthew manning <mbhmanning@gmail.com>
2016-07-17 17:38:52 +1200

    regular commit

--- AccelRead.c
+++ AccelRead.c
@@ -1,25 +1,52 @@
-#include <i2c.h>
+#include "i2c.h"
 #include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
 
 #define X_ACCEL_REG 0x3b
-#define ACCEL_DIVSOR 8192
+#define ACCEL_ADDRESS 0x68
+#define ACCEL_DIVISOR 8192
 #define RANGE_CON_REG 0x1c
 #define RANGE_CON_VAL 0x08
 
 int i;
+struct i2c_handle * DevHandle;
+uint8_t Buf16b[2];
+uint16_t U16val;
+int16_t  S16val;
+float Accel;
+float Divisor;
 
-
-//open i2c device
-
-//write config register 
-
-for(i = 0; i < 100 ;i++)
+int main(void)
 {
-	//read x acceleration
+	Divisor = ACCEL_DIVISOR / 9.806; 
+
+	//open i2c device
+	DevHandle = i2c_init(1, ACCEL_ADDRESS, 0);
 	
-	//sensible value
+	//write range config register 
+	Buf16b[0] = RANGE_CON_VAL;
+	i2c_write_to_reg(DevHandle, RANGE_CON_REG, Buf16b, 1);
 	
-	//actual reg values
 	
+	for(i = 0; i < 100 ;i++)
+	{	
+		//read x acceleration
+		i2c_read_from_reg(DevHandle, X_ACCEL_REG, Buf16b, 2);
 		
+		//sensible value
+		U16val = ((uint16_t)Buf16b[0] << 8) | ((uint16_t)Buf16b[1]);
+		S16val = (int16_t)U16val;
+		
+		Accel = S16val / Divisor;
+		
+		printf("acceleration is %0.3f m/s/s\n", Accel);
+		
+		//actual reg values
+		printf("actual reg values are: %x and %x\n", Buf16b[0], Buf16b[1]);
+		usleep(100000);//100 ms
+			
+	}
+	
+return 0;
 }
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 #build and link AccelRead.c
 
-
+ arm-linux-gnueabi-gcc -Wall AccelRead.c i2c.c -o AccelRead
