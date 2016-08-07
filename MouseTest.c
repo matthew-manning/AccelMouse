404c4fd66ec7b5eb09e6001448b506aaabcc2dfd
matthew manning <mbhmanning@gmail.com>
2016-08-07 16:14:56 +1200

    regular commit

--- MouseTest.c
+++ MouseTest.c
@@ -21,24 +21,24 @@ int main(void)
 	for(i =0; i < 20000; i++) //should be around 20 sec
 	{
 		//get time
-		/*
+		
 		clock_gettime(CLOCK_MONOTONIC, &TimeStamp);
-		RefTime = (TimeStamp.tv_sec*1000)+(TimeStamp.tv_nsec * 1000000);
+		RefTime = (TimeStamp.tv_sec*1000)+(TimeStamp.tv_nsec / 1000000);
 		printf("RefTime is %0.2f ms\n", RefTime);
-		*/
+		
 		updateSpeeds( &TestMouse);
 		
 		printf("acceleration(m/s/s) x:%0.2f y:%0.2f z:%0.2f\nvelocity(m/s) x:%0.2f y:%0.2f z:%0.2f\n\n",
 				TestMouse.DevHandle.AccX, TestMouse.DevHandle.AccY ,TestMouse.DevHandle.AccZ,
 				TestMouse.VelX, TestMouse.VelY, TestMouse.VelZ);
 				
-	/*	while(CurTime < RefTime + CYCLIC_PERIOD)//burn till 10 ms later
+		while(CurTime < RefTime + CYCLIC_PERIOD)//burn till 10 ms later
 		{
 			clock_gettime(CLOCK_MONOTONIC, &TimeStamp);
-			CurTime = (TimeStamp.tv_sec*1000)+(TimeStamp.tv_nsec * 1000000);
+			CurTime = (TimeStamp.tv_sec*1000)+(TimeStamp.tv_nsec / 1000000);
 
-		} */
-		usleep(1000000);//for debugging		
+		} 
+
 
 	}
 	return 0;
